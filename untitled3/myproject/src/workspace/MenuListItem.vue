a700f2714ae38716be6920c35752d58de5d80577
ljm <ljm3307@163.com>
2020-08-18 09:21:12 +0800

    个人工作台交互

--- untitled3/myproject/src/workspace/MenuListItem.vue
+++ untitled3/myproject/src/workspace/MenuListItem.vue
@@ -1,25 +1,28 @@
 <template>
- <div class="goods-item"  >
-   <el-card class="box-card" shadow="always">
-     <div style="float: right">
-       <el-dropdown >
-        <span class="el-dropdown-link">
-         <i class="el-icon-setting el-icon--right"></i>
-        </span>
-         <el-dropdown-menu slot="dropdown">
-           <el-dropdown-item ><el-button type="text" @click="back">恢复</el-button></el-dropdown-item>
-           <el-dropdown-item divided><el-button type="text" @click="dlt">删除</el-button></el-dropdown-item>
-         </el-dropdown-menu>
-       </el-dropdown>
-     </div>
-     <!-- <img :src="TemItem.img" alt=""> -->
-     <img class="mainImg" src="../assets/文档.svg" />
+  <div class="goods-item">
+    <el-card class="box-card" shadow="always">
+      <div style="float: right">
+        <el-dropdown>
+          <span class="el-dropdown-link">
+            <i class="el-icon-setting el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>
+              <el-button type="text" @click="back">恢复</el-button>
+            </el-dropdown-item>
+            <el-dropdown-item divided>
+              <el-button type="text" @click="dlt">删除</el-button>
+            </el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+      <!-- <img :src="TemItem.img" alt=""> -->
+      <img class="mainImg" src="../assets/文档.svg" />
 
-     <div class="goods-info">
-       <p>{{MenuItem.title}}</p>
-     </div>
-
-   </el-card>
+      <div class="goods-info">
+        <p>{{MenuItem.title}}</p>
+      </div>
+    </el-card>
   </div>
 </template>
 
@@ -30,20 +33,19 @@ export default {
   name: "MenuListItem",
   inject: ["reload"],
   data() {
-    return{
-    }
+    return {};
   },
   props: {
     MenuItem: {
-        type: Object,
-        default() {
-          return {}
-        }
-      }
+      type: Object,
+      default() {
+        return {};
+      },
     },
+  },
   methods: {
     itemClick() {
-       // let data = {
+      // let data = {
       //   DocID: this.docsItem.docID,
       //   UserID: this.UserID,
       // };
@@ -63,7 +65,7 @@ export default {
       this.$router.push("/detail/" + this.docsItem.docID);
     },
     back() {
-     const h = this.$createElement;
+      const h = this.$createElement;
       this.$msgbox({
         title: "提示",
         message: h("p", null, [
@@ -175,51 +177,50 @@ export default {
           });
         });
     },
-    }
+  },
 };
 </script>
 
-<style>
-  .el-dropdown-link {
-    cursor: pointer;
-    color: #3778ff;
-  }
-  .el-icon-arrow-down {
-    font-size: 8px;
-  }
-  .goods-item {
-    margin-top: 20px;
-    margin-right: 38px;
-    position: relative;
-    width:220px;
-    height: 180px;
-    margin-bottom: 10px;
-  }
+<style scoped>
+.el-dropdown-link {
+  cursor: pointer;
+  color: #3778ff;
+}
+.el-icon-arrow-down {
+  font-size: 8px;
+}
+.goods-item {
+  margin-top: 20px;
+  margin-right: 38px;
+  position: relative;
+  width: 220px;
+  height: 180px;
+  margin-bottom: 10px;
+}
 
-  .mainImg {
-    width: 70px;
-    border-radius: 20px;
-    margin-left: 30%;
-    margin-top: 8px;
-  }
+.mainImg {
+  width: 70px;
+  border-radius: 20px;
+  margin-left: 30%;
+  margin-top: 8px;
+}
 
-  .goods-info {
-    font-size: 13px;
-    position: relative;
-    margin-top: 5px;
-    left: 0;
-    right: 0;
-    overflow: hidden;
-    text-align: center;
-  }
+.goods-info {
+  font-size: 13px;
+  position: relative;
+  margin-top: 5px;
+  left: 0;
+  right: 0;
+  overflow: hidden;
+  text-align: center;
+}
 
-  .goods-info p {
-    font-size: 18px;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    margin-bottom: 2px;
-    padding-bottom: -10px;
-  }
-
+.goods-info p {
+  font-size: 18px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  margin-bottom: 2px;
+  padding-bottom: -10px;
+}
 </style>
